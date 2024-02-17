7285e4eeed80c7c56c33172dce7dcf9d8e2ebe74
HarshitPachori <harshitpachori345@gmail.com>
2024-02-17 15:56:30 +0530

    created express server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 // require("dotenv").config({ path: "./env" });
+import { app } from "./app.js";
 import connectDB from "./db/db_connect.js";
 import dotenv from "dotenv";
+
 dotenv.config({
   path: "./env",
 }); // if using this theen use -r dotenv/config --experimental-json-modules
-connectDB();
+
+connectDB()
+  .then(() => {
+    const port = process.env.PORT || 8000;
+    app.listen(port, () => {
+      console.log(`Server is running at Port : ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`MongoDB connection failed !!`);
+  });
