35f627ff556b30821c8e0d8bb8a06dcedaf8496b
HarshitPachori <harshitpachori345@gmail.com>
2024-02-18 16:28:13 +0530

    added feature for file upload on cloudinary

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,4 +1,5 @@
 import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -6,3 +7,22 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    // if no file then simply return
+    if (!localFilePath) return;
+    // upload file to cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    //  now file uploaded successfully and we have to remove file from local server
+    fs.unlinkSync(localFilePath);
+    return response;
+  } catch (error) {
+    // if got error then remove file saved in local
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
