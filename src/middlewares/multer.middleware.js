feeba999c7cb7fc00bb7e7971bf3ec488bdbe0af
HarshitPachori <harshitpachori345@gmail.com>
2024-02-20 15:52:47 +0530

    minor bug fix

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -5,7 +5,7 @@ const storage = multer.diskStorage({
     cb(null, "public/temp");
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname + "-" + Math.round(Math.random() * 1e9));
+    cb(null, file.fieldname + "-" + Math.round(Math.random() * 1e9));
   },
 });
 
